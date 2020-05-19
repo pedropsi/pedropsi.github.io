92ca8e6be3e45e74db127b81039ca67e2dcb4f5b
pedropsi <36260325+pedropsi@users.noreply.github.com>
2020-05-19 08:58:28 +0100

    Auto Dependencies I

--- codes/data-transfer.js
+++ codes/data-transfer.js
@@ -5250,6 +5250,74 @@ Introspect=function(){
 	return Introspect.list;
 }
 
+var JavascriptFunctions=["RegExp","Array","Object","Date","String","Number","Set","Math"];
+
+
+FunctionSourceTokens=function(F){
+	if(!F)
+		return [];
+
+	var source=F.toString();
+	var separator="SecretSePaRaToR"; //must not be confoundable
+	var symbols=Tokens().split("").concat([" ","\n","\t","\r"]); //things to ignore
+
+	var tokens=StringReplace(F.toString(),symbols.map(t=>[t,separator])).split(separator);
+	tokens=tokens.filter(t=>t&&t[0].toLowerCase()!==t[0])
+	tokens=Complement(tokens,JavascriptFunctions);
+	
+	return tokens;
+}
+
+FunctionDirectDependencies=function(F){
+	var Fname=IsString(F)?F:FunctionName(F);
+	var F=IsString(F)?globalThis[F]:F;
+
+	if(!FunctionDirectDependencies.o)
+		FunctionDirectDependencies.o={};
+
+	var dependencies=FunctionDirectDependencies.o;
+
+	if(dependencies[Fname])
+		return dependencies[Fname];
+
+	return dependencies[Fname]=FunctionSourceTokens(F);
+}
+
+var FunctionDependents={};
+AddDependent=function(ascendent,dependent){
+	if(!FunctionDependents[ascendent])
+		FunctionDependents[ascendent]=[dependent];
+	else if(!In(FunctionDependents[ascendent],dependent))
+		FunctionDependents[ascendent].push(dependent);
+	
+	return FunctionDependents[ascendent];
+}
+
+FunctionDependencies=function(F){
+
+	var dependencies=FunctionDirectDependencies(F);
+	var newd=dependencies.map(FunctionDirectDependencies).flat();
+		newd=Complement(newd,dependencies);
+		dependencies=Union(newd,dependencies);
+	while(newd.length>0){
+		newd=newd.map(FunctionDirectDependencies).flat();
+		newd=Complement(newd,dependencies);
+		dependencies=Union(newd,dependencies);
+	}
+
+	var Fname=IsString(F)?F:FunctionName(F);
+	dependencies.map(d=>AddDependent(d,Fname))
+
+	if(FunctionDependencies[Fname])
+		return FunctionDependencies[Fname];
+
+	return FunctionDependencies[Fname]=dependencies;
+}
+
+FunctionImportance=function(F){
+	return FunctionDependencies(F).length;
+}
+
 CoreFunctionNames=[
 	"In","InString","InArrayOrObj",
 	"Capture",
