0a31b0b46f65a26929db907a3adde159f2b9d257
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-12 11:02:33 +0100

    Update puzzle-type.js

--- codes/game/puzzle-type/puzzle-type.js
+++ codes/game/puzzle-type/puzzle-type.js
@@ -2548,8 +2548,6 @@ function InitialiseGameCanvas(){
 	OpenElement(GameMiddleHTML(),"gameCanvas");
 	LaunchTouchActions(".middle",TouchActionsMiddle());
 	Listen("mouseup",CopyHandler(LetterExtractor("#letters")),"#letters");
-	
-	//AddElement(WinPaneHTML(),"gameCanvas")
 }
 
 
@@ -2758,8 +2756,8 @@ function LevelLoadMacro(){
 			CleanLevel();
 			ColoriseGameBar();//Change colour each level
 		}},
-		{Starter:TransitionNotesIn,endDelay:200},
 		...LevelTopInMacro(),
+		{Starter:TransitionLettersIn,endDelay:200},
 		{Starter:UnBlockInput}
 	]
 };
@@ -2771,10 +2769,9 @@ function LevelWinMacro(){
 		{Starter:function(){
 			FadeElement(".top .notes",200);
 			FadeElement(".top .keystrokes",200);
-			Class(".top",".downwards",200);
+			Class(".top",".downwards");
 		},endDelay:200},
 		...GoalMatchedMacro(),
-		//{Starter:()=>UpdateWinPane(CurLevelTitle()),endDelay:1000},
 		{Starter:function(){
 			UnClass(".top",".downwards");
 			NextLevel();//may need delay
@@ -2808,7 +2805,7 @@ function GoalMatchedMacro(){
 			RemoveElements(".shrinked");
 		},endDelay:100},
 		BorderlessAction({endDelay:1000}),
-		MirrorAction({endDelay:1000}),
+		MirrorAction({endDelay:Letters().length*100}),
 		{Starter:function(){
 			UnClass(".middle #letters","downwards");
 		}}
@@ -3384,64 +3381,3 @@ function TutorialMacro(){
 }
 
 
-// function WinPaneHTML(){
-// 	return `<div class='winpane'></div>`;
-// }
-
-// var WinTexts={
-// 	"Direct":"Good!",				
-// 	"Reverse":".iceN",
-// 	"Follow":"Go on!",
-// 	"Consonant":"C_NGR_TS!",
-// 	"Second":"Well done. Well done.",
-// 	"Rotate":"Ecxeellnt",
-// 	//"Oppose":???,
-// 	"Rise":"Sky-high.",
-// 	"Falls":"Refreshing!",
-// 	"Precedent":"Unprecedented!",		
-// 	"Teleporter":"Far-fetched!",		
-// 	"Superior":"Superb!",			
-// 	//"Tangles":???,
-// 	"Difference":"Distinction!",
-// 	//"Photocopier":???,
-// 	"Symmetries":"Spec(tac)ular",
-// 	"Fillet":"You made the cut!",
-// 	"Topological":"Logically!",
-// 	"Wasd":"aMAZEing...",
-// 	"Nokia 1998":"A classic!",
-// 	"Dvorak":"Puzzle Typing since 1932",
-// 	"ひらがな":"驚くばかり",
-// 	"Nigeria":"Nawa oh!",
-// 	"Anagram":"MAGNificient!",
-// 	"Genetic.":"It's in your genes!",
-// 	"Ironclad":"Elementary!",
-// 	"Deaf":"Ace Badge#",
-// 	"⠍⠕⠗⠎⠑":"⠁⠺⠑⠐⠎",
-// 	"Dividi":"Ipsum bonum!",
-// 	"Magnetism":"Electrifying!",
-// 	//"Fuchsia":"Stunning!",
-// 	"Odd":"EVEN better!",
-// 	"Latent clones":"FOURmidable!",
-// 	"Shepherdess hence unladylike":"cHERiSHEd!",
-// 	"Starting buds":"Startling!",
-// 	"La rapide surprise":"Très bien!",
-// 	"Just cut and paste":"Very very very good.",
-// 	"Order is all":"Steadfast!"
-// };
-//Extraordinary, Stupendous, fabulous, Astounding, captivating,Wondrous, Unbelievable, breathtaking
-
-// function UpdateWinPane(title){
-// 	RemoveChildren(".winpane");
-// 	if(In(WinTexts,title))
-// 		var text=WinTexts[title];
-// 	else
-// 		var text="Well done!";
-
-// 	var winElements="12345678".split("").map(n=>`<div class="winlegend">${n==2?text:""}</div>`).join("\n")
-// 	ReplaceChildren(winElements,".winpane");
-
-// 	setTimeout(()=>UnHideElement(".winpane"),250);
-// 	setTimeout(()=>ToggleClass(".winpane","animated"),500);
-// 	setTimeout(()=>HideElement(".winpane"),3000);
-
-// }
