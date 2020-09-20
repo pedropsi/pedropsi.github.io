3be72d5bc256d6789fea57b040b1eac8efd4dcaa
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-20 18:37:02 +0100

    ILM

--- codes/game/modules/data-game-extras.js
+++ codes/game/modules/data-game-extras.js
@@ -170,12 +170,6 @@ if(typeof ObtainKeyboardTarget==="undefined")
 	}
 
 
-var ObtainInterlevelMessage=False;
-if(!Portable())
-	ObtainInterlevelMessage=True;
-if(ConsoleExternal())
-	ObtainInterlevelMessage=False;
-
 var MainKeys={
 	"undo":"Z",
 	"redo":"Y",
@@ -784,7 +778,7 @@ function MarkWonLevel(lvl){
 function NextLevel(){
 	var curscreen=Min(CurrentScreen(),LastScreen()?LastScreen():CurrentScreen());
 	CurrentScreen(curscreen);
-	HideLevelMessage();
+	
 	ResetStory();
 	
 	if (TitleScreen())
@@ -1119,11 +1113,6 @@ function UnstarLevel(l){
 	return Number(l.replace("★","").replace("☆",""));
 }
 
-function UpdateAccessLevelMessage(){
-	ReplaceChildren(ChosenLevelDescriptionHTML(),".question");
-}
-
-Listen("Set level",UpdateAccessLevelMessage);
 
 function LevelNumberFromTotal(lvl){
 	return PadLevelNumber(lvl)+"/"+MaxLevel()+LevelHintStar(lvl)
@@ -1644,21 +1633,6 @@ function CloseKeyboard(){
 	if(CurrentDatapack().buttonSelector==="KeyboardButton")
 		CloseCurrentDatapack();
 	GameFocus();
-}
-
-////////////////////////////////////////////////////////////////////////////////
-//Better inter-level messages
-function ShowLevelMessage(lvlscreen){
-	if(!lvlscreen)
-		lvlscreen=CurrentScreen();
-	
-	HideLevelMessage();
-	AppendElement("<div class='game-message-container'><div class='game-message'><p>"+CurrentScreenMessage()+"</p></div></div>",gameSelector);
-	GameFocus();
-}
-
-function HideLevelMessage(){
-	RemoveElements('.game-message-container');
 }
 
 
