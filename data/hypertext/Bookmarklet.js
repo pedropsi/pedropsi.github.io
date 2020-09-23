3e1a54577e6822243138c3f402af482e2bae3088
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 13:37:02 +0100

    hyper bkm

---
+++ data/hypertext/Bookmarklet.js
@@ -0,0 +1,12 @@
+HyperText("Bookmarklet",function(){
+	return `
+		<h3>In Chrome</h3>
+		<p>${KB("Drag")} the above bookmarklet link to your bookmarks toolbar.</p>
+		
+		<h3>In Firefox</h3>
+		<p>${KB("Right click")} on the above bookmarklet link, then select "bookmark this link".</p>
+
+		<h3>If nothing else works</h3>
+		<p>Replace the content of an old bookmark with the above link content. Or you could run the bookmarklet js code directly in a game page (it'd still be quicker than manual alternatives).</p>
+	`
+})
