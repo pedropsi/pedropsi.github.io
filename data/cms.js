b9757ed91727bc3945332a3ebaac732c35727b4d
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 16:36:25 +0100

    narrowgrid

--- data/cms.js
+++ data/cms.js
@@ -295,19 +295,6 @@ CMSObject=function(title){
 
 // Keyboard Instuctions
 
-ArrowGridKB=function(direction,i){
-	return `<kbd class="key-${Directions[i]}">${ObtainSymbol(direction)}</kbd>`;
-}	
-ArrowKeysGridHTML=function(LURDArray){
-	return `<span class="grid controls-arrow">${LURDArray.map(ArrowGridKB).join("")}</span>`;
-}
-ArrowKeysHyperText=function(){
-	var grids=[
-		ArrowKeysGridHTML(Directions),
-		ArrowKeysGridHTML(["A","W","D","S"]),
-		ArrowKeysGridHTML(Directions.map(d=>Prefix(d,"swipe")))];
-	return	Enumerate(grids,"or");
-}
 
 
 ExportCMSProperties=function(){
