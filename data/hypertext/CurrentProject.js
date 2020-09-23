4eb6f4753d94f5a9e3babc6c81c5de2f1ac92380
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 23:47:30 +0100

    Update CurrentProject.js

--- data/hypertext/CurrentProject.js
+++ data/hypertext/CurrentProject.js
@@ -2,7 +2,7 @@ HyperText("CurrentProject",function(){return AHTML(`
 	<div>
 		<p><b>Puzzle Type</b> - coming soon!</p>
 		<p>An inductive typing puzzle for desktop.</p>
-		${ButtonHTML({txt:"Yes please!"})}
+		${ButtonHTML({txt:"Learn more"})}
 	</div>`
 	,"puzzle-type.html",{class:"announce"})
 })
