8dfe662454b3b57cee86e3cd8e0b06047743a0ea
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 12:52:12 +0100

    Update worldmap.js

--- data/worldmap.js
+++ data/worldmap.js
@@ -1,13 +1,3 @@
-/*     xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <path id="MyPath"
-          d="M 100 200 
-             C 200 100 300   0 400 100
-             C 500 200 600 300 700 200
-             C 800 100 900 100 900 100" />
-  </defs>
-
-*/
 
 var worldMap=
 `<svg id="worldMap" 
