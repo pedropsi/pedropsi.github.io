c838147696850c9677e79bd93838049fc2c1ec5c
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 23:51:52 +0100

    Subscribe

---
+++ data/hypertext/Subscribe.js
@@ -0,0 +1,7 @@
+HyperText("Subscribe",function(){return AHTML(`
+	<div>
+		<p>Learn about ${v.NAME()}'s next project!</p>
+		${ButtonHTML({txt:"Subscribe"})}
+	</div>`
+	,"subscribe.html",{class:"announce"})
+})
