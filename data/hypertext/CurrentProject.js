3b2bc64bca0bc6f9dd915e8d4856db946c8f9de4
Pedro PSI <pedropsi@users.noreply.github.com>
2020-09-23 23:46:14 +0100

    Announce

---
+++ data/hypertext/CurrentProject.js
@@ -0,0 +1,8 @@
+HyperText("CurrentProject",function(){return AHTML(`
+	<div>
+		<p><b>Puzzle Type</b> - coming soon!</p>
+		<p>An inductive typing puzzle for desktop.</p>
+		${ButtonHTML({txt:"Yes please!"})}
+	</div>`
+	,"puzzle-type.html",{class:"announce"})
+})
