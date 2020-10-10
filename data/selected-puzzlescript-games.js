f75aeb9e0ed6634cba8add17110dd45f2f58d310
Pedro PSI <pedropsi@users.noreply.github.com>
2020-10-10 09:43:28 +0100

    Update selected-puzzlescript-games.js

--- data/selected-puzzlescript-games.js
+++ data/selected-puzzlescript-games.js
@@ -3,7 +3,8 @@
 */
 
 var SelectedGames=[
-	{LINK:`https://chemelia.itch.io/goosey-gardens`,TITLE:`Goosey Gardens`,AUTHOR_LINK:`https://twitter.com/chapliboy`,AUTHOR:`Amelia Moser (Chemelia)`,YEAR:2020,TAGS:["Garden","Gravity","Sokoban"]},
+	{LINK:`https://galactical.itch.io/wall-clip`,TITLE:`Wall Clip`,AUTHOR_LINK:`https://galactical.itch.io`,AUTHOR:`Galactical`,YEAR:2020,TAGS:["Wraparound","Sokoban"]},
+	{LINK:`https://chemelia.itch.io/goosey-gardens`,AUTHOR_LINK:`https://chemelia.itch.io`,TITLE:`Goosey Gardens`,AUTHOR:`Amelia Moser (Chemelia)`,YEAR:2020,TAGS:["Garden","Gravity","Sokoban"]},
 	{LINK:`https://nebu-soku.itch.io/gyoretu-puzzle`,TITLE:`Gyoretu Puzzle`,AUTHOR_LINK:`https://twitter.com/chapliboy`,AUTHOR:`Nebu Soku`,YEAR:2020,TAGS:["Swarm"]},
 	{LINK:`https://playtest.chapliboy.com/puzzlescript.html`,TITLE:`Mouse in slippers`,AUTHOR_LINK:`https://twitter.com/chapliboy`,AUTHOR:`Chapliboy`,YEAR:2020,TAGS:["Sokoban","Ice"]},
 	{LINK:`https://jacklance.github.io/MacAndCHI/`,TITLE:`Mac and CHI`,AUTHOR_LINK:`https://twitter.com/Jack_L_Lance`,AUTHOR:`Jack Lance & PHI`,YEAR:2020,TAGS:["Sokoban","Team"]},
