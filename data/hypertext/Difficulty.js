3b573b4805661220186bc959b9eec0bffc4dff88
Pedro PSI <pedropsi@users.noreply.github.com>
2020-10-20 13:49:21 +0100

    diff

---
+++ data/hypertext/Difficulty.js
@@ -0,0 +1,3 @@
+HyperText("Difficulty",function(){return `
+<p>Level difficulty is indicated by the number of stars, in a scale of 1 ${ObtainSymbol("asterisk-heavy")} - very easy - to 5 ${ObtainSymbol("asterisk-heavy").repeat(5)} - very hard.</p>
+`})
