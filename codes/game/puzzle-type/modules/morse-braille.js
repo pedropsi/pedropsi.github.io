c92874e4bcd0d1955ef74cf77f36d15c7ef81d6f
Pedro PSI <pedropsi@users.noreply.github.com>
2020-11-15 01:26:26 +0000

    Update morse-braille.js

--- codes/game/puzzle-type/modules/morse-braille.js
+++ codes/game/puzzle-type/modules/morse-braille.js
@@ -133,3 +133,52 @@ var BrailleSorted=[
 "⠽",
 "⠾",
 "⠿"];
+
+/*
+var Permutators={
+	"vertical":function(hsize){
+		return function(string){
+			var s=string.split("");
+			var vsize=Round(s.length/hsize);
+			for(var i=0;i<s.length;i++)
+				s[i]=string[(vsize-Round(i/hsize))*hsize+i%hsize];
+			return s;
+		}
+	},
+	"horizontal":function(hsize){
+		return function(string){
+			var s=string.split("");
+			for(var i=0;i<s.length;i++){
+				s[i]=string[Round(i/hsize)*hsize+hsize-i%hsize];
+			}
+			return s;
+		}
+	},
+	"inversion":function(hsize){
+		return function(string){
+			var s=string.split("");
+			for(var i=0;i<s.length;i++){
+				if(i%(2*hsize)>=hsize)
+					s[i]=string[Round(i/hsize)*hsize+hsize-i%hsize];
+			}
+			return s;
+		}
+	},
+	"modular":function(hsize){
+		return function(string){
+			var s=string.split("");
+			for(var i=0;i<s.length;i++){
+				if(i%2*hsize>=hsize)
+					s[i]=string[Round(i/hsize)*hsize+hsize-i%hsize];
+			}
+			return s;
+		}
+	}
+}
+
+function Permutator(text,opts){
+	var string=text;
+	if(opts.flipvertical&&opts.vsize)
+		string
+}
+*/
