0698946a5292255234f7e70b269c6b166aa04d45
Pedro PSI <pedropsi@users.noreply.github.com>
2020-11-22 18:05:27 +0000

    morsesearch

---
+++ codes/game/puzzle-type/modules/morse-braille-search.js
@@ -0,0 +1,18 @@
+Variants
+// "...-.--..-/---....--./.-.-.-.---" Line,LR:TB "I awe at me. I wink." (I awe at me. I emit art)(station i weary)
+// ".-.-.-.---/---....--./...-.--..-" line,LR:BT
+
+// ".-.-.-.---/.--....---/...-.--..-" line,LRzig:BT (art jade aging)
+// "...-.--..-/.--....---/.-.-.-.---" line,LRzig:TB "Statue me I am art."
+
+
+// "..--.-/.--..-/.-...-/-..-.-/.--.--" Char,LR,TB:LR  (a army set me up)
+// ".---../.--..-/.-...-/.-.--./---..-" Char,LR,BT:LR  (age pale, ten matte)(a gun, a rite, yet me)(am i a tear in yet me) (am I a teal art tag) (at tie gala a wet me / a wag)
+// ".-..--/.-.-.-/...-.-/-...--/.---.-" Char,TB,LR:LR  (enact law, i wag)(enact laws go)
+// ".-.--./.-.-.-/...-.-/..---./--.-.-" Char,BT,LR:LR  (awe art, vex me)(ape talent I opt)(a week askew yet)
+
+// ".-..--/.-.-.-/...-.-/..-.--/--.-.-" Char,BTzigzag,LR:LR  (ran tale kite ok)
+// ".-.--./.-.-.-/...-.-/-..--./.---.-" Char,TBzig,LR:LR  (apart i awe a name) (a win true mute) (a week rite meet team) (a petal, a we, a team)
+
+// "...-.--..-/./---.-.-.-./---....--" Linearound,LR:TB  (i awe roar tame)
+// ".-.-.-.---/./-..--.-.../---....--" Linearound,LR:BT  (i awe roar tame)
