96aae593149e7763399ac7fe896c84c4c56d1191
Pedro PSI <36260325+pedropsi@users.noreply.github.com>
2021-01-31 22:38:19 +0000

    hints images

--- data/hypertext/License.js
+++ data/hypertext/License.js
@@ -2,13 +2,13 @@ HyperText("License",function(){return Licenses.join(`
 `)})
 
 var Licenses={//People
-	"MIT":{LINK:"https://opensource.org/licenses/MIT"},
-	"CC-BY-4.0":{LINK:"https://creativecommons.org/licenses/by/4"},
-	"CC-BY-SA-4.0":{LINK:"https://creativecommons.org/licenses/BY_nc_SA/4.0"},
+	"MIT":{url:"https://opensource.org/licenses/MIT"},
+	"CC-BY-4.0":{url:"https://creativecommons.org/licenses/by/4"},
+	"CC-BY-SA-4.0":{url:"https://creativecommons.org/licenses/BY_nc_SA/4.0"},
 }
 
 LicenseHTML=function(linkObj,name){
-	return AHTML(name,linkObj.LINK);
+	return AHTML(name,linkObj.url);
 }
 	
 RegisterLicense=function(key){
--- data/store.js
+++ data/store.js
@@ -7,15 +7,10 @@ var Store={
 	TAGLINE:()=>"an inductive typing  puzzle",
 	CALL:()=>"Find out whether you're the <b>Puzzle type</b>!",
 	DETAILS:()=>`<p>Desktop version.</p><p>All information in the official ${AHTML("puzzle-type")} page.</p>`,
-	LINK:()=>"puzzle-type",
+	LINK:"puzzle-type",
 	IMAGE_EXT:()=>"gif",
 	IMAGE_ALT:()=>"Puzzle type",
-	PRODUCT_KEY:()=>"sku_GE9y0FHJHS0rtN",
-	STRIPE_KEY:()=>"pk_test_w0G0ENTYZ67hs4uUfdO7HYcp00whShrwUN",
-	SUCCESS_LINK:()=>"https://pedropsi.github.io/puzzle-type",
-	ERROR_LINK:()=>"https://pedropsi.github.io/puzzle-type",
 	ACTION_BUTTON:v.BUTTON_SOON,
-	OWL:()=>"78269703/8AB38B13",
 	INTEGRATION:()=>""
 	},
 "gift":{
@@ -26,12 +21,10 @@ var Store={
 	TAGLINE:()=>"a token of appreciation",
 	CALL:()=>"Spread the good karma.",
 	DETAILS:()=>`<p>What is in the box?</p>`,
-	LINK:()=>"store",
+	LINK:"store",
 	CALL:()=>`Send ${v.NAME()} a small gift!`,
 	IMAGE_ALT:()=>"Present",
 	ACTION_BUTTON:v.BUTTON_SOON,
-	OWL:()=>"78269976/1FDC655D",
-	INTEGRATION:()=>""
 	}
 }
 
@@ -44,7 +37,7 @@ function StoreHTML(){
 function StoreImageHTML(obj){
 	var id=GenerateId();
 	var size=180;
-	var src=ImagePath(obj.LINK(),obj.IMAGE_EXT()||v.IMAGE_EXT(),size);
+	var src=ImagePath(obj.LINK,obj.IMAGE_EXT()||v.IMAGE_EXT(),size);
 	LazyImageLoader(id,src);							
 	return `<img id="${id}" 
 				class="image" 
@@ -67,42 +60,9 @@ function ProductHTML(ProdObj){
 			<div class="product-call text">${ProdObj.CALL()}</div>
 			${ProdObj.ACTION_BUTTON(ProdObj)}
 		</div>
-		${ProdObj.INTEGRATION(ProdObj)}
 	</div>`
 }
 
-function StripeButtonHTML(ProdObj){
-	return `
-	<div id="checkout_BUTTON-${ProdObj.PRODUCT_KEY()}" class="button centered buy" tabindex="0">
-		${ProdObj.ACTION()} (${ProdObj.PRICE()})
-	</div>`;
-}
-
-function StripeIntegrationHTML(ProdObj){
-return `
-<div id="error-message-${ProdObj.PRODUCT_KEY()}"></div>
-<script>
-	function Integrate_${ProdObj.PRODUCT_KEY()}(){
-		var stripe=Stripe('${ProdObj.STRIPE_KEY()}');
-		var checkoutButton=document.getElementById('checkout-button-${ProdObj.PRODUCT_KEY()}');
-		checkoutButton.addEventListener('click',function(){
-			stripe.redirectToCheckout({
-				items:[{sku:'${ProdObj.PRODUCT_KEY()}',quantity:1}],
-				successUrl:'${ProdObj.SUCCESS_LINK()}?source=stripe&result=success',
-				cancelUrl:'${ProdObj.ERROR_LINK()}?source=stripe&result=failure'
-			})
-			.then(function(result){
-				if(result.error){
-					var displayError=document.getElementById('error-message-${ProdObj.PRODUCT_KEY()}');
-					displayError.textContent=result.error.message;
-				}
-			});
-		});
-	};
-	
-	DelayUntil(function(){return typeof Stripe!=="undefined";},Integrate_${ProdObj.PRODUCT_KEY()});
-	</script>`;
-}
 
 if(DATA){DATA["Store"]=Store}else{var DATA={"Store":Store}}
 Shout("store");
