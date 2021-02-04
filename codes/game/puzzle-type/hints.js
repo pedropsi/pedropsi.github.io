4d5088190a3130866a35f7146c2503581b6846fd
Pedro PSI <36260325+pedropsi@users.noreply.github.com>
2021-02-04 18:53:22 +0000

    Update hints.js

--- codes/game/puzzle-type/hints.js
+++ codes/game/puzzle-type/hints.js
@@ -13,7 +13,8 @@ var HintsObject={
 		"What happens after you type a consonant? And a vowel?"
 	],
 	"Second":[
-		"The word 'second' has more than one meaning..."
+		"The word 'second' has many meanings... which one matters?",
+		"When does a letter disappear?"
 	],
 	"Rotate":[
 		"What is rotating?",
@@ -22,7 +23,7 @@ var HintsObject={
 	],
 	"Rise":[
 		"What is rising?",
-		"Can you make use of the rise?"
+		"Since you can't prevent the rise, better use it!"
 	],
 	"Falls":[
 		"What is falling?",
@@ -34,57 +35,62 @@ var HintsObject={
 		"What does 'precede' mean?",
 		"How could you protect some of the letters?"
 	],
- 	"Superior":[
-		"After you type a letter, where does the caret move?",
-		"What does 'superior' mean, in the context of a moving caret?",
-		"Why does the caret move to the end of the word, sometimes?",
-		"How would you set up the letters, to later place the caret wherever you want?"
-	],
-	"Difference":[
-		"When does the caret jump forward, and when backwards?",
-		"In which situations does the caret stay in place?",
-		"How many steps does the caret jump? Can you control this?",
-		"Which letter combinations trap the caret in a cycle?"
-	],
 	"Symmetries":[
-		"What can happens to the order of the already typed letters?",
-		"Which letters can't you type? What do they do?",
+		"Which letters can you type?",
+		"Which letters <b>can't</b> you type? What are their special powers?",
+		"What can happens to the order of the typed letters?",
 		"About which symmetries are we thinking?"
 	],
 	"Fillet":[
-		"Most words can't be cut in the way 'fillet' does. This was a rare find.",
+		"Most words can't be sliced like <em>FILLET</em>. This is a rare, high-quality word.",
 		"After typing 6 letters, you may go back to the beginning to fix any mistakes.",
 		"In how many different ways can you make a single letter?"
 	],
+	"Reshape":[
+		"How do letters reshape themselves each turn?",
+		"Once a letter reshapes, can it recover the original shape? Are all letters alike?",
+		"When you can't recover the original shape, what else can you try?",
+	],
 	"Topological":[
-		"Topology is an area of mathematics about the continuity of geometrical shapes. Here, our shapes are letters.",
+		"Topology is an area of mathematics about the continuity of geometrical shapes.",
 		"Letters belong to different 'Homeomorphism' classes. Within the same class, letters can be deformed without gluing or cutting arms.",
 		"So, you cannot cut an <b>O</b> into a <b>C</b> (different classes), but you can straighten a <b>C</b> into an <b>L</b> (same class).",
 		"Adding an arm to an <b>I</b> to make a <b>Y</b> is also not allowed, as they belong to different classes.",
 		"Can you split all 26 letters into different classes?"
 	],
 	"Nokia 1998":[
-		"Nokia phones in 1998... How nostagic!",
-		"How did people back then type on their phones?"
+		"Nokia phones in 1998... Nostalgic!",
+		"How did people back then type text messages?",
+		"Do grab an old phone if you don't know or remember!"
 	],
 	"Wasd":[
 		"Which letters can you type? What do they do?",
 		"Where will you write WASD?",
 		"Do draw a map if you need one!"
-		],
+	],
 	"Dvorak":[
 		"Dvorak does not refer to Antonín Dvořák, the great composer.",
 		"It refers to the <b>standard</b> Dvorak keyboard layout!"
 		],
+	"РУССКАЯ":[
+		"What is the name of this alphabet? Which countries use it?",
+		"Is there any relation between this alphabet and the latin one?"
+	],
 	"ひらがな":[
 		"Which language are we looking at?",
 		"Which writing system are we facing?",
-		"Which sound does each kana represent? How many different kanas belong to this writing sytem?"
+		"Which sound does each kana represent? How many different kanas belong to this writing system?"
 	],
 	"Nigeria":[
 		"Have you opened the world map?",
 		"How are countries geographically related?",
 		"Are countries the only entities we need?"
+	],
+	"Polaris Australis":[
+		"It's night and you look at the sky. What do you see?",
+		"Where is Polaris? Where is Polaris Australis?",
+		"How are stars grouped?",
+		"How are stars ordered within each constellation?"
 	],
 	"Genetic.":[
 		"What does genetic refer to?",
@@ -94,12 +100,18 @@ var HintsObject={
 	],
 	"Anagram":[
 		"When can we write a permanent letter?",
-		"Which of the written letters becomes permanent?"
+		"Which of the written letters becomes permanent?",
+		"Can you repeat anagrams? Better be creative!"
 	],
 	"Ironclad":[
 		"What is iron, chemically?",
 		"Where do you find all other elements?",
-		"Can you find a D?"
+		"For the last element, Mendeleiev cannot help you."
+	],
+	"White chocolate mint":[
+		"These three words have more in common than you may think at first.",
+		"Have you ever <em>seen</em> white mint chocolate? Was it colourful?",
+		"If can write white, write all other colours."
 	],
 	"Deaf":[
 		"Have you turned on the sound?",
@@ -107,53 +119,82 @@ var HintsObject={
 		"How do we name a group of three notes played together?",
 		"Do you hear the difference between small and capitalised letters?"
 	],
-	"⠍⠕⠗⠎⠑":[
+	"Enactlawsmama":[
 		"Which languages are written here?",
-		"Do dots mean anything in those languages?"
+		"Do dots mean anything in those languages?",
+		"How about the dashes?"
 	], 
-	"Magnetism":[
-		"Which letters behave oddly together?",
-		"What do they represent?"
-	],
-	"Dividi":[
-		"Which letters can you type?",
-		"When do typed letters behave abnormally?",
-		"What does the word 'dividi' mean and in which language?"
-	],
-	"Odd":[
-		"What is odd about this level?",
-		"Have you <em>even</em> tried?"
-	],
 	"Latent clones":[
 		"Which words produce effect?",
 		"Can you time such effects perfectly?",
 		"How much can you recede, before advancing?"
 	],
 	"Shepherdess hence unladylike":[
-		"Which words produce effect?",
-		"Which <b>other</b> words could produce the same effect?"
+		"Which words produce effects?",
+		"Which <b>other</b> words produce effect?"
 	],
-	"Starting buds":[
-		"With which letters can you start, and why?",
-		"Add a new letter. What happens to the relative position of the other letters? Look closely. Try longer words too.",
-		"If you are still tempted to search for anagrams, see hint #2.",
-		"The dictionary ignores many words: slang, dialect, abbreviations, names of people, places, companies, etc...",
-		"A space is no different from a new letter: Before it all was valid, after it all must be valid too.",
-		"The last hint was revealing too much. So it was eliminated, letter by letter."
+	"Today doves flit before winter":[
+		"What is the main theme?",
+		"Do you see how this theme affects different words?"
 	],
 	"La rapide surprise":[
 		"Which languages are we using?",
-		"When is a translation triggered?"
+		"When is a translation triggered?",
+		"Please pay attention to the <em>conjugation des verbes</em>!",
+	],
+	"Starting buds":[
+		"Why can you start with some letters but not others?",
+		"Where can you add new letters? Are anagrams valid? Look closely!",
+		"The dictionary ignores many words, such as slang, dialect, abbreviations, names of people, places and  companies.",
+		"A space has the same effect as any letter: valid words before and after.",
+		"Sorry! As this last hint revealed too much, I deleted it, one letter at the time."
+	],
+	"Teleporter":[
+		"When does the cursor teleport?",
+		"Where does it teleport to? What if there are many such letters?",
+		"Can you prepare a safe teleporting path?"
+	],
+	"Superior":[
+		"What does <em>SUPERIOR</em> mean?",
+		"After you type a letter, where does the caret move?",
+		"Why does the caret move to the end of the word, sometimes?",
+		"How would you prepare the letters, to place the caret wherever you wish?"
+	],
+	"Difference":[
+		"When does the caret jump forward, and when backwards?",
+		"In which situations does the caret stay in place?",
+		"How many steps does the caret jump? Can you control this?",
+		"Which letter combinations trap the caret in a cycle?"
+	],
+	"Loosely less":[
+		"Try to look at this level from a different angle.",
+		"Do you have a calculator with you?",
+		"Let's be clear about the boundaries. Numbers end where letters start.",
+		"How low can you go?",
+	],
+	"Magnetism":[
+		"Which letters behave oddly together?",
+		"What do they represent?"
+	],
+	"Odd":[
+		"What is <em>odd</em> about this level?",
+		"Look: have you <em>even</em> tried?"
 	],
 	"Just cut and paste":[
 		"Which keywords matter?",
 		"Are keywords always triggered?",
-		"How do you ensure two keywords are not triggered?"
+		"How do you block a keyword trigger?"
 	],
 	"Order is all":[
 		"This level was inspired by the game Baba is you, by Hempuli.",
 		"There is only one keyword: is.",
-		"You may use the keyword as often as you need, to make many rules."
+		"You may use the keyword as often as you need, to make many rules...",
+		"...the cool thing is, you only need one!"
+	],
+	"Dividi":[
+		"Which letters can you type?",
+		"What does the word 'dividi' mean and in which language?",
+		"When do typed letters trigger an effect?"
 	]
 };
 
